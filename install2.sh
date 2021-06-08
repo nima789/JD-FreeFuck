a2337869238a3def00185caefa2808fdb655462d
nima789 <77829771+nima789@users.noreply.github.com>
2021-06-08 14:15:41 +0800

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -130,7 +130,7 @@ function ProjectDeployment() {
     ## 定义全局变量
     echo "export JD_DIR=$BASE" >>/data/data/com.termux/files/usr/etc/profile
     source /data/data/com.termux/files/usr/etc/profile
-    wget -p https://raw.gitwj.workers.dev/nima789/JD-FreeFuck/part2/mb.sh /data/data/com.termux/files/home
+    wget https://raw.gitwj.workers.dev/nima789/JD-FreeFuck/part2/mb.sh
 }
 
 ## 判定控制面板安装结果：
