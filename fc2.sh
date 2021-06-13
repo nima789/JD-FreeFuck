becac60d2da40e4318dcad14182e17eb57580a88
nima789 <77829771+nima789@users.noreply.github.com>
2021-06-13 16:13:16 +0800

    Update and rename install2.sh to fc2.sh

--- fc2.sh
+++ fc2.sh
@@ -3,9 +3,9 @@
 ## 安装目录
 BASE="/data/data/com.termux/files/home/jd"
 ## 项目分支
-JD_BASE_BRANCH="jd_base"
+JD_BASE_BRANCH="master"
 ## 项目地址
-JD_BASE_URL="https://github.com/nima789/JD-FreeFuck.git"
+JD_BASE_URL="git@jd_base_gitee:supermanito/jd_base.git"
 ## 活动脚本库私钥
 JD_KEY_BASE="/data/data/com.termux/files/home/.ssh"
 JD_KEY_URL="https://raw.gitwj.workers.dev/nima789/JD-FreeFuck/part2/.ssh2/"
